90de9d9b6099b288d1506afadc49306b7f56c675
Donnahue George <Donnahuegjr@gmail.com>
2021-04-08 21:49:31 -0400

    initalizing firebase with our application

---
+++ client/src/firebase.js
@@ -0,0 +1,15 @@
+import firebase from firebase/app
+import "firebase/auth";
+import "firebase/firestore"
+
+var firebaseConfig = {
+    apiKey: process.env.REVITALIZE_API,
+    authDomain: process.env.REVITALIZE_AUTH,
+    projectId: process.env.REVITALIZE_PROJECT,
+    storageBucket: process.env.REVITALIZE_STORAGE,
+    messagingSenderId: process.env.REVITALIZE_MESSAGE,
+    appId: process.env.REVITALIZE_APPID,
+    measurementId: process.env.REVITALIZE_MEASUREMENT
+  };
+  
+  firebase.initializeApp(firebaseConfig);
