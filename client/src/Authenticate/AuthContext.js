2320d90dbbac56f8182ffb8f2a74b2388a74ca60
Donnahue George <Donnahuegjr@gmail.com>
2021-04-10 13:32:46 -0400

    getting authentication to work

---
+++ client/src/Authenticate/AuthContext.js
@@ -0,0 +1,30 @@
+import React, { useContext, useEffect, useState } from 'react';
+import { auth } from '../firebase';
+
+const AuthContext = React.createContext();
+
+export function useAuth() {
+  return useContext(AuthContext);
+}
+
+export function AuthProvider({ children }) {
+  const [currentUser, setCurrentUser] = useState();
+
+  function Signup(email, password) {
+    return auth.createUserWithEmailAandPassword(email, password);
+  }
+  useEffect(() => {
+    const userSet = auth.onAuthStateChanged(user => {
+      setCurrentUser(user);
+    });
+    return userSet;
+  });
+
+  const value = {
+    currentUser,
+    Signup,
+  };
+  return (
+    <AuthProvider.Provider value={value}>{children}</AuthProvider.Provider>
+  );
+}
--- client/src/component/signup.js
+++ client/src/component/signup.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { useAuth } from '../Authenticate/AuthContext';
 export default function Signup() {
   return (
     <div>
