5723f9626a6ece519dae02660967e53612ae2926
Donnahue George <Donnahuegjr@gmail.com>
2021-04-10 13:52:45 -0400

    connecting firebase auth servers to our repo

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,18 +1,17 @@
-import firebase from firebase/app
-import "firebase/auth";
-import "firebase/firestore"
+import firebase from 'firebase/app';
+import 'firebase/auth';
 
 var app = {
-    apiKey: process.env.REVITALIZE_API,
-    authDomain: process.env.REVITALIZE_AUTH,
-    projectId: process.env.REVITALIZE_PROJECT,
-    storageBucket: process.env.REVITALIZE_STORAGE,
-    messagingSenderId: process.env.REVITALIZE_MESSAGE,
-    appId: process.env.REVITALIZE_APPID,
-    measurementId: process.env.REVITALIZE_MEASUREMENT
-  };
-  
-  firebase.initializeApp(app);
+  apiKey: process.env.REVITALIZE_API,
+  authDomain: process.env.REVITALIZE_AUTH,
+  projectId: process.env.REVITALIZE_PROJECT,
+  storageBucket: process.env.REVITALIZE_STORAGE,
+  messagingSenderId: process.env.REVITALIZE_MESSAGE,
+  appId: process.env.REVITALIZE_APPID,
+  measurementId: process.env.REVITALIZE_MEASUREMENT,
+};
 
- 
-  export default app
+firebase.initializeApp(app);
+
+export const auth = app.auth;
+export default app;
