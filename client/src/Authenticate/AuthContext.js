73306ac1d680f2df14c4a126fbf710faad117712
Donnahue George <Donnahuegjr@gmail.com>
2021-04-14 11:18:36 -0400

    got laoding state on current user

--- client/src/Authenticate/AuthContext.js
+++ client/src/Authenticate/AuthContext.js
@@ -9,6 +9,7 @@ export function useAuth() {
 
 export function AuthProvider({ children }) {
   const [currentUser, setCurrentUser] = useState();
+  const [loading, setLoading] = useState(true);
 
   function Signup(email, password) {
     return auth.createUserWithEmailAndPassword(email, password);
@@ -17,6 +18,7 @@ export function AuthProvider({ children }) {
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged(user => {
       setCurrentUser(user);
+      setLoading(false);
     });
 
     return unsubscribe;
@@ -26,5 +28,9 @@ export function AuthProvider({ children }) {
     currentUser,
     Signup,
   };
-  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+  return (
+    <AuthContext.Provider value={value}>
+      {!loading && children}
+    </AuthContext.Provider>
+  );
 }
