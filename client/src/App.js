dd1edc56627c566d87a0688f55aede69cb67b047
Donnahue George <Donnahuegjr@gmail.com>
2021-04-14 12:54:45 -0400

    changing components and created login page

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,21 @@ import React, { Component } from 'react';
 import './App.css';
 import Signup from './component/Signup2.0';
 import { AuthProvider } from './Authenticate/AuthContext';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Main_Page from './component/Main_Page';
+import Login from './component/Login2.0';
 class App extends Component {
   render() {
     return (
-      <AuthProvider>
-        <Signup />
-      </AuthProvider>
+      <Router>
+        <AuthProvider>
+          <Switch>
+            <Route exact path="/" component={Main_Page} />
+            <Route path="/signup" component={Signup} />
+            <Route path="/login" component={Login} />
+          </Switch>
+        </AuthProvider>
+      </Router>
     );
   }
 }
