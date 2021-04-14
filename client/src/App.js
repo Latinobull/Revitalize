71757a225ecd9480a8c806e0780861cf8d2f7b3b
Esiena Ekwofia <eekwofia@gmail.com>
2021-04-13 20:53:05 -0400

    fixing api file

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Signup from './component/Signup';
+console.log(process.env.REACT_APP_API_KEY)
 
 class App extends Component {
   render() {
--- client/src/api/recipes.js
+++ client/src/api/recipes.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const APP_ID = `fff68517`;
+const APP_ID = "fff68517";
 const APP_KEY = "7f55af49684ad7830f7be236dd395a50";
 
 const url = `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`;
