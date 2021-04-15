1347db083ad75104a916999d43112a86c6b9e159
Esiena Ekwofia <eekwofia@gmail.com>
2021-04-15 19:35:56 -0400

    working on receipe api

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import { AuthProvider } from './Authenticate/AuthContext';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Main_Page from './component/Main_Page';
 import Login from './component/Login2.0';
+import Searchbar from "./component/Recipes/Searchbar"
 class App extends Component {
   render() {
     return (
@@ -14,6 +15,7 @@ class App extends Component {
             <Route exact path="/" component={Main_Page} />
             <Route path="/signup" component={Signup} />
             <Route path="/login" component={Login} />
+            <Route path="/recipes" component={Searchbar} />
           </Switch>
         </AuthProvider>
       </Router>
