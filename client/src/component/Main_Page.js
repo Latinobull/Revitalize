27b3a71979f56fa760c589a63ded7c343fb47262
Donnahue George <Donnahuegjr@gmail.com>
2021-04-15 19:03:52 -0400

    added a log  out function

--- client/src/component/Main_Page.js
+++ client/src/component/Main_Page.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useHistory } from 'react-router';
+import { useAuth } from '../Authenticate/AuthContext';
 
 export default function Main_Page() {
-  function handleLogut() {}
+  const [error, setError] = useState('');
+  const { Logout } = useAuth();
+  const history = useHistory();
+
+  async function handleLogut() {
+    setError('');
+
+    try {
+      await Logout();
+      history.push('/login');
+    } catch {
+      setError('Something went wrong');
+    }
+  }
   return (
     <div>
       <h1>Revitalize</h1>
       <button onClick={handleLogut}>Log Out</button>
+      {error && <h5>{error}</h5>}
     </div>
   );
 }
