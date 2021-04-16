26c216a537e571a4d202f8167e249434cf1eb70e
Donnahue George <Donnahuegjr@gmail.com>
2021-04-15 20:01:25 -0400

    preparing for user to change display name

--- client/src/component/Main_Page.js
+++ client/src/component/Main_Page.js
@@ -4,7 +4,7 @@ import { useAuth } from '../Authenticate/AuthContext';
 
 export default function Main_Page() {
   const [error, setError] = useState('');
-  const { Logout } = useAuth();
+  const { Logout, currentUser } = useAuth();
   const history = useHistory();
 
   async function handleLogut() {
@@ -17,8 +17,16 @@ export default function Main_Page() {
       setError('Something went wrong');
     }
   }
+  var userDisplay = JSON.stringify(currentUser.displayName);
+
   return (
     <div>
+      {currentUser.displayName ? (
+        <h2>{userDisplay}</h2>
+      ) : (
+        <h2>Welcome new User, please click here to make a new Display Name</h2>
+      )}
+
       <h1>Revitalize</h1>
       <button onClick={handleLogut}>Log Out</button>
       {error && <h5>{error}</h5>}
