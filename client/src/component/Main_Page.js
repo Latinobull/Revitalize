294c688fbfe5a9f63472effb4383917f394c19e5
Donnahue George <Donnahuegjr@gmail.com>
2021-04-15 20:18:53 -0400

    allowing user to change display name

--- client/src/component/Main_Page.js
+++ client/src/component/Main_Page.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import { useHistory } from 'react-router';
 import { useAuth } from '../Authenticate/AuthContext';
 
@@ -6,6 +6,7 @@ export default function Main_Page() {
   const [error, setError] = useState('');
   const { Logout, currentUser } = useAuth();
   const history = useHistory();
+  const displayNameRef = useRef();
 
   async function handleLogut() {
     setError('');
@@ -19,16 +20,37 @@ export default function Main_Page() {
   }
   var userDisplay = JSON.stringify(currentUser.displayName);
 
+  async function handleNameChange() {
+    setError('');
+    try {
+      await currentUser.updateProfile({
+        displayName: displayNameRef.current.value,
+      });
+    } catch {
+      setError('Something went wrong changing your name');
+    }
+  }
+
   return (
     <div>
+      {console.log(currentUser)}
       {currentUser.displayName ? (
-        <h2>{userDisplay}</h2>
+        <h2>Welcome Back{userDisplay}</h2>
       ) : (
         <h2>Welcome new User, please click here to make a new Display Name</h2>
       )}
-
       <h1>Revitalize</h1>
       <button onClick={handleLogut}>Log Out</button>
+      <br />
+      <form onSubmit={handleNameChange}>
+        <input
+          type="name"
+          name="DisplayName"
+          placeholder="DisplayName"
+          ref={displayNameRef}
+        ></input>
+        <button type="submit">Change User</button>
+      </form>
       {error && <h5>{error}</h5>}
     </div>
   );
