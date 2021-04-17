2ae9a17ddd25af87c3b6c0c93ac3a13260e70c84
Esiena Ekwofia <eekwofia@gmail.com>
2021-04-17 01:34:19 -0400

    building on recipe api, able to get data to append to page

---
+++ client/src/component/Recipes/RecipeCard.js
@@ -0,0 +1,120 @@
+import React from "react";
+import { makeStyles } from '@material-ui/core/styles';
+import clsx from 'clsx';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Collapse from '@material-ui/core/Collapse';
+import Avatar from '@material-ui/core/Avatar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import { green} from '@material-ui/core/colors';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import ShareIcon from '@material-ui/icons/Share';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+// import Searchbar from "./Searchbar";
+// import API from "../../api/recipes";
+import {v4 as uuidv4} from "uuid"
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 0,
+    paddingTop: '56.25%', // 16:9
+  },
+  expand: {
+    transform: 'rotate(0deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.shortest,
+    }),
+  },
+  expandOpen: {
+    transform: 'rotate(180deg)',
+  },
+  avatar: {
+    backgroundColor: green[500],
+  },
+}));
+
+export default function RecipeReviewCard({recipe}) {
+  const classes = useStyles();
+  const [expanded, setExpanded] = React.useState(false);
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+  const kalories= recipe.recipe.calories.toFixed(2)
+  return (
+    <Card className={classes.root}>
+      <CardHeader
+        avatar={
+          <Avatar aria-label="recipe" className={classes.avatar}>
+            R
+          </Avatar>
+        }
+        action={
+          <IconButton aria-label="settings">
+            <MoreVertIcon />
+          </IconButton>
+        }
+        title={recipe.recipe.label}
+        subheader= {kalories}
+      />
+      <CardMedia
+        className={classes.media}
+        image={recipe.recipe.image}
+        title={recipe.recipe.label}
+      />
+      <CardContent>
+        <Typography variant="body2" color="textSecondary" component="p">
+        
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing>
+        <IconButton aria-label="add to favorites">
+          <FavoriteIcon />
+        </IconButton>
+        <IconButton aria-label="share">
+          <ShareIcon />
+        </IconButton>
+        <IconButton
+          className={clsx(classes.expand, {
+            [classes.expandOpen]: expanded,
+          })}
+          onClick={handleExpandClick}
+          aria-expanded={expanded}
+          aria-label="show more"
+        >
+          <ExpandMoreIcon />
+        </IconButton>
+      </CardActions>
+      <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <CardContent>
+          <Typography paragraph>Ingredients</Typography>
+          <Typography paragraph>
+            {recipe.recipe.ingredients.map((ingredient) =>{
+              return(
+                <li key={uuidv4()} className="listofIngredients">{ingredient.text}</li>
+              )
+            })}
+          </Typography>
+          <Typography paragraph>
+          
+          </Typography>
+          <Typography paragraph>
+           
+          </Typography>
+          <Typography>
+          </Typography>
+        </CardContent>
+      </Collapse>
+    </Card>
+  );
+}
