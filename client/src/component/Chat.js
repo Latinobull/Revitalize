981b7d5a10561d4835a10b89c3af5dd088f14aa3
Donnahue George <Donnahuegjr@gmail.com>
2021-04-17 15:00:49 -0400

    got it all to display. now to properly organize the file structure

--- client/src/component/Chat.js
+++ client/src/component/Chat.js
@@ -1,24 +1,50 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useStore } from '../Authenticate/Firestore';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
 export default function Chat() {
   //   const { Test } = useStore();
   const [error, setError] = useState();
+  const [messages, setMessages] = useState([]);
+  const db = firebase.firestore();
+  const query = db.collection('messages').orderBy('createdAt').limit(100);
+  useEffect(() => {});
+  useEffect(() => {
+    // Subscribe to query with onSnapshot
+    const unsubscribe = query.onSnapshot(querySnapshot => {
+      // Get all documents from collection - with IDs
+      const data = querySnapshot.docs.map(doc => ({
+        ...doc.data(),
+        id: doc.id,
+      }));
+      // Update state
+      setMessages(data);
+    });
 
-  async function handleTest(e) {
-    e.preventDefault();
+    // Detach listener
+    return unsubscribe;
+  }, []);
+  //   async function handleTest(e) {
+  //     e.preventDefault();
 
-    // try {
-    //   setError('');
-    //   await Test();
-    //   console.log('it worked');
-    // } catch {
-    //   setError('Something went wrong');
-    // }
-  }
+  // try {
+  //   setError('');
+  //   await Test();
+  //   console.log('it worked');
+  // } catch {
+  //   setError('Something went wrong');
+  // }
+  //   }
   return (
-    <form onSubmit={handleTest}>
+    <form>
       <label>Welcome</label>
       <button>Do you work?</button>
+      <ul>
+        {messages.map(message => (
+          <li key={message.id}>{message.text}</li>
+        ))}
+      </ul>
       {error && <h4>{error}</h4>}
     </form>
   );
