597651da4f8d206047bb3055d70c865d81437381
Donnahue George <Donnahuegjr@gmail.com>
2021-04-20 00:09:18 -0400

    getting ready to write data to the database

--- client/src/component/Chat.js
+++ client/src/component/Chat.js
@@ -1,8 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import 'firebase/firestore';
 import firebase from 'firebase/app';
 export default function Chat() {
   const [messages, setMessages] = useState([]);
+  const [newMessage, setNewMessage] = useState('');
+  const inputRef = useRef();
   const db = firebase.firestore();
   const query = db.collection('messages').orderBy('createdAt').limit(100);
   useEffect(() => {
@@ -20,15 +22,30 @@ export default function Chat() {
     // Detach listener
     return unsubscribe;
   }, []);
+
+  const handleOnChange = e => {
+    setNewMessage(e.target.value);
+  };
+
   return (
-    <form>
-      <label>Welcome</label>
-      <button>Do you work?</button>
+    <div>
       <ul>
         {messages.map(message => (
           <li key={message.id}>{message.text}</li>
         ))}
       </ul>
-    </form>
+      <form onSubmit>
+        <input
+          ref={inputRef}
+          type="text"
+          value={newMessage}
+          onChange={handleOnChange}
+          placeholder="Type your message here..."
+        />
+        <button type="submit" disabled={!newMessage}>
+          Send
+        </button>
+      </form>
+    </div>
   );
 }
