a7702d51d6924a2ff41ccfb053a6c85b20982968
Esiena Ekwofia <eekwofia@gmail.com>
2021-04-20 20:47:30 -0400

    added benefits of meditation and images

--- client/src/component/Meditation/index.js
+++ client/src/component/Meditation/index.js
@@ -1,7 +1,14 @@
 import React from "react";
 import "./style.css";
 import { Grid } from "@material-ui/core";
-import anxiety from "../../assets/images/stress.jpeg"
+import stressimg from "../../assets/images/stressimg.jpeg";
+import canxiety from "../../assets/images/canxiety.jpeg";
+import emotional from "../../assets/images/emotional.jpeg";
+import selfimage from "../../assets/images/selfimage.jpeg";
+import attention from "../../assets/images/attention.webp";
+import sleep from "../../assets/images/sleep.jpeg";
+import bloodpressure from "../../assets/images/bloodpressure.jpeg";
+import pain from "../../assets/images/pain.jpeg";
 
 function MeditationFacts() {
 
@@ -15,83 +22,59 @@ function MeditationFacts() {
   justify="center"
   alignItems="center"
 >
-{/* <div className="post-wrap"> 
+<div className="post-wrap"> 
        <div className="post"> 
            <div className="post-front">
-               <div className="post-img" id="post-img1"></div>
-               <div className="post-info">
-                   <span>June 25, 2020</span>
-                   <h2>black black black </h2>
-               </div>
+           <img src= {stressimg} alt="woman biting penci" className="practice"/>
+           <h3>Reduce Stress</h3>
            </div>
            <div className="post-back">
                <div className="post-except">
-                   <h2>Testing </h2>
-                   <img src= {anxiety}/>
-                   By learning to calm your body and mind, your physical and emotional stress can melt away. This leaves you feeling better, refreshed, and ready to face the challenges of your day with a healthy attitude.
-                   
+                   <p>By learning to calm your body and mind, your physical and emotional stress can melt away. This leaves you feeling better, refreshed, and ready to face the challenges of your day with a healthy attitude. </p><br></br>
+                   <a href="https://www.healthline.com/nutrition/12-benefits-of-meditation#1.-Reduces-stress">Read More</a>
                </div>
-               <a href="https://www.healthline.com/nutrition/12-benefits-of-meditation#2.-Controls-anxiety">Read More</a>
            </div>
        </div>
    </div>
    <div className="post-wrap"> 
        <div className="post"> 
            <div className="post-front">
-               <div className="post-img" id="post-img2"></div>
-               <div className="post-info">
-                   <span>June 25, 2020</span>
-                   <h2>black black black </h2>
-               </div>
+           <img src= {canxiety} alt="man resting head in hands" className="practice"/>
+           <h3>Controls Anxiety</h3>
            </div>
            <div className="post-back">
                <div className="post-except">
-                   <h2>Promotes Emotional Health</h2>
-                   <p>
-                   Some forms of meditation can improve depression and reduce negative thoughts. It may also decrease levels of inflammatory cytokines, which could contribute to depression.
-                   </p>
+                   <p>Anxiety is a cognitive state connected to an inability to regulate emotions. But research shows that a consistent meditation practice reprograms neural pathways in the brain and, therefore, improves our ability to regulate emotions.</p><br></br>
+                   <a href="https://www.healthline.com/nutrition/12-benefits-of-meditation#2.-Controls-anxiety">Read More</a>
                </div>
-               <a href="https://www.healthline.com/nutrition/12-benefits-of-meditation#3.-Promotes-emotional-health">Read More</a>
            </div>
        </div>
    </div>
    <div className="post-wrap"> 
        <div className="post"> 
            <div className="post-front">
-               <div className="post-img" id="post-img3"></div>
-               <div className="post-info">
-                   <span>June 25, 2020</span>
-                   <h2>black black black </h2>
-               </div>
+           <img src= {emotional} alt="woman smiling" className="practice"/>
+           <h3>Promotes Emotional Health</h3>
            </div>
            <div className="post-back">
                <div className="post-except">
-                   <h2>Reduce Stress</h2>
-                   <p>
-                   By learning to calm your body and mind, your physical and emotional stress can melt away. This leaves you feeling better, refreshed, and ready to face the challenges of your day with a healthy attitude.
-                   </p>
+                  <p>Some forms of meditation can improve depression and reduce negative thoughts. It may also decrease levels of inflammatory cytokines, which could contribute to depression.</p> <br></br>
+                  <a href="https://www.healthline.com/nutrition/12-benefits-of-meditation#3.-Promotes-emotional-health">Read More</a>
                </div>
-               <a href="https://www.healthline.com/nutrition/12-benefits-of-meditation#1.-Reduces-stress">Read More</a>
            </div>
        </div>
    </div>
    <div className="post-wrap"> 
        <div className="post"> 
            <div className="post-front">
-               <div className="post-img" id="post-img1"></div>
-               <div className="post-info">
-                   <span>June 25, 2020</span>
-                   <h2>black black black </h2>
-               </div>
+           <img src= {attention} alt="man laying with head down" className="practice"/>
+           <h3>Lengthens Attention Span</h3>
            </div>
            <div className="post-back">
                <div className="post-except">
-                   <h2>Reduce Stress</h2>
-                   <p>
-                   By learning to calm your body and mind, your physical and emotional stress can melt away. This leaves you feeling better, refreshed, and ready to face the challenges of your day with a healthy attitude.
-                   </p>
+                 <p>Several types of meditation may build your ability to redirect and maintain attention. Meditating for just 10 minutes every day can help prevent your mind from wandering and reduce repetitive, anxious thoughts.</p> <br></br>
+                 <a href="https://www.healthline.com/nutrition/12-benefits-of-meditation#5.-Lengthens-attention-span">Read More</a>
                </div>
-               <a href="https://www.healthline.com/nutrition/12-benefits-of-meditation#1.-Reduces-stress">Read More</a>
            </div>
        </div>
    </div>
@@ -99,28 +82,61 @@ function MeditationFacts() {
    <div className="post-wrap"> 
        <div className="post"> 
            <div className="post-front">
-               <div className="post-img" id="post-img1"></div>
-               <div className="post-info">
-                   <span>June 25, 2020</span>
-                   <h2>black black black </h2>
-               </div>
+           <img src= {selfimage} alt="woman looking in mirror" className="practice"/>
+           <h3>Enhances Self-Awareness</h3>
            </div>
            <div className="post-back">
                <div className="post-except">
-                   <h2>Reduce Stress</h2>
-                   <p>
-                   By learning to calm your body and mind, your physical and emotional stress can melt away. This leaves you feeling better, refreshed, and ready to face the challenges of your day with a healthy attitude.
-                   </p>
+                   <p>Self-inquiry and related styles of meditation can help you “know yourself.” This can be a starting point for making other positive changes. In order to become more self-aware, you need to stop thinking and stop doing. Meditation is the perfect solution for this.</p><br></br>
+                   <a href="https://www.healthline.com/nutrition/12-benefits-of-meditation#4.-Enhances-self-awareness">Read More</a>
                </div>
-               <a href="https://www.healthline.com/nutrition/12-benefits-of-meditation#1.-Reduces-stress">Read More</a>
            </div>
        </div>
-   </div> */}
+   </div>
 
+   <div className="post-wrap"> 
+       <div className="post"> 
+           <div className="post-front">
+           <img src= {sleep} alt="woman sleeping" className="practice"/>
+           <h3>Improves Sleep</h3>
+           </div>
+           <div className="post-back">
+               <div className="post-except">
+                   <p>A variety of meditation techniques can help you relax and control runaway thoughts that can interfere with sleep. This can shorten the time it takes to fall asleep and increase sleep quality.</p><br></br>
+                   <a href="https://www.healthline.com/nutrition/12-benefits-of-meditation#9.-Improves-sleep">Read More</a>
+               </div>
+           </div>
+       </div>
+   </div>
+   <div className="post-wrap"> 
+       <div className="post"> 
+           <div className="post-front">
+           <img src= {bloodpressure} alt="blood pressure machine" className="practice"/>
+           <h3>Decrease Blood Pressure</h3>
+           </div>
+           <div className="post-back">
+               <div className="post-except">
+                   <p>Blood pressure decreases not only during meditation but also over time in individuals who meditate regularly. This can reduce strain on the heart and arteries, helping prevent heart disease.</p><br></br>
+                   <a href="https://www.healthline.com/nutrition/12-benefits-of-meditation#11.-Can-decrease-blood-pressure">Read More</a>
+               </div>
+           </div>
+       </div>
+   </div>
+   <div className="post-wrap"> 
+       <div className="post"> 
+           <div className="post-front">
+           <img src= {pain} alt="wonan grabbing neck and back" className="practice"/>
+           <h3>Helps Control Pain</h3>
+           </div>
+           <div className="post-back">
+               <div className="post-except">
+                   <p>Meditation can diminish the perception of pain in the brain. When you meditate, you release endorphins which are natural pain relievers. This may help treat chronic pain when used to supplement medical care or physical therapy. In addition, muscles and tissues around your joints are more relaxed, and your brain can be in a calmer state so you'll feel less pain.</p><br></br>
+                   <a href="https://www.healthline.com/nutrition/12-benefits-of-meditation#10.-Helps-control-pain">Read More</a>
+               </div>
+           </div>
+       </div>
+   </div>
      </Grid>
-<wrapper className="practice-wrapper">
-    <img src={anxiety} className="practice"/>
-    </wrapper>
      </div>
     )
 }
