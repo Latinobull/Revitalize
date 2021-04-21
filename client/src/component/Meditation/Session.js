b70287bf3452662103098dd3d3dd10bc2eaca208
Esiena Ekwofia <eekwofia@gmail.com>
2021-04-20 22:15:41 -0400

    installed react progress bar and added page for progress bar

---
+++ client/src/component/Meditation/Session.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
+
+
+function Session (){
+return (
+
+<div></div>
+)
+
+}
