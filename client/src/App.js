c5775628e9b1a2a0efa6a5f7c660ed3c2adf376c
Donnahue George <Donnahuegjr@gmail.com>
2021-04-21 10:56:11 -0400

    fix merge

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,17 @@
-import React, { Component } from "react";
-import "./App.css";
-import Signup from "./component/Signup2.0";
-import { AuthProvider } from "./Authenticate/AuthContext";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Main_Page from "./component/Main_Page";
-import Login from "./component/Login2.0";
-import Recipes from "./component/Recipes/Recipes";
-import Journals from "./component/Journals/index";
-import PrivateRoute from "./component/PrivateRoute";
-import Appbar from "./component/Appbar";
+import React, { Component } from 'react';
+import './App.css';
+import Signup from './component/Signup2.0';
+import { AuthProvider } from './Authenticate/AuthContext';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Main_Page from './component/Main_Page';
+import Login from './component/Login2.0';
+import Recipes from './component/Recipes/Recipes';
+import Journals from './component/Journals/index';
+import PrivateRoute from './component/PrivateRoute';
+import Appbar from './component/Appbar';
 import Chat from './component/Chat';
-<<<<<<< HEAD
 import Profile from './component/Profile';
-=======
-import MeditationFacts from "./component/Meditation"
->>>>>>> 48e6f082da52009c2ece438acf08f5d69867dbcf
+import MeditationFacts from './component/Meditation';
 
 class App extends Component {
   render() {
