9029208d63bbccbf742271515159695830696aca
Adia Boyd <72044418+codeyab22@users.noreply.github.com>
2021-04-22 20:58:33 -0400

    yoga.js
    I put my authentication key to enable me to use the Youtube videos for the yoga API call. I added console.log(GetResult) between lines 12 and 13 to get a response.

---
+++ client/src/api/yoga.js
@@ -0,0 +1,103 @@
+//Authenication key to gain access to Youtube
+
+var youtube_Key = "AIzaSyD2M93FGbul72E4O3Fnu4rzh6QFmA106E4";
+
+// Access to the json yoga API
+function listOfAllYogaPoses(){
+  fetch('https://raw.githubusercontent.com/rebeccaestes/yoga_api/master/yoga_api.json')
+  .then(result =>
+    result.json())
+  .then(newResult => {
+    getVariableforAllPoses(newResult)
+    var newResult = newResult;
+  })
+  .catch(error =>
+    console.log(error))
+}
+
+// Access to Youtube API
+function callYoutubeAPI(valueSelected){
+  fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&q=how+to+do+${valueSelected}+yoga+pose&maxResults=1&&safeSearch=moderate&key=${youtube_Key}`)
+  .then(youtubeResult =>
+  console.log(newResult)
+    youtubeResult.json())
+  .then(youtubeResult => {
+    displayOtherResults(youtubeResult.items[0]);
+  })
+  .catch(error =>
+    console.log(error))
+}
+
+// Making a dropdown menu
+function getVariableforAllPoses(newResult){
+  for(let i=0; i<newResult.length; i++){
+    let allPoses = newResult[i].english_name;
+    $('.select-dropdown').append(
+      `
+      <option class="dropdown-style" id="move-dropdown" value="${allPoses}">
+        ${allPoses}
+      </option>
+      `)
+    if(allPoses === newResult[i].english_name){
+      let sanskritName = newResult[i].sanskrit_name;
+      let poseImage = newResult[i].img_url;
+    }
+  }
+  submitButton(newResult);
+}
+
+// CTA BUTTON
+function submitButton(newResult){
+  $('.dropdown-form').submit(event => {
+    event.preventDefault();
+    let valueSelected = $('.dropdown-style:selected').val();
+    callYoutubeAPI(valueSelected);
+    for(let i=0; i<newResult.length; i++){
+      if(valueSelected === newResult[i].english_name){
+        let objectSelected = newResult[i];
+        cssChanges();
+        displayResults(objectSelected);
+      }
+    }
+  });
+}
+
+//JQUERY CHANGES TO CSS
+function cssChanges(){
+  $('.light').hide();
+  $("#app-explanation").hide();
+  $('#intro-background').hide();
+  $('body').css('backgroundImage','url(' + 'https://images.unsplash.com/photo-1526376043067-5af36c35cd6c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60' + ')');
+  $('body').css({
+    'backgroundColor':'rgba(0,0,0,0.4)',
+    'backgroundRepeat':'repeat',
+    'backgroundSize':'cover',
+  });
+
+}
+
+//RESULTS DISPLAYED -YOGA
+function displayResults(objectSelected){
+  $('.results').empty();
+  $('.results').append(`
+      <div class='position-rel'>
+      <div id="sanskrit-div"><p>${objectSelected.sanskrit_name}</p></div>
+      </div>
+      <div class='results-inline'>
+      <img id="pose-img" src="${objectSelected.img_url}"></img>
+      </div>
+    `)
+}
+
+//Displayed Results on YouTube
+function displayOtherResults(video){
+  $('.results-inline').append(`
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/${video.id.videoId}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    `)
+}
+
+//to begin 
+function init() {
+	listOfAllYogaPoses();
+
+
