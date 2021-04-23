4c9ef93f6fe08122da426134bf5c77b4f325789a
Local Al <alexander.s.woolley@gmail.com>
2021-04-23 14:03:34 -0600

    menu rework and add button to iterate through quotes

--- client/src/component/Journals/index.js
+++ client/src/component/Journals/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import JournalMenu from "./JournalMenu";
 import JournalCard from "./Card/index";
-import SubmitButton from './SubmitButton';
 import Banner from "./Banner/index";
+import InspireButton from "./Banner/Button";
 
 
 function Journals() {
   return (
     <>
    <Banner/>
+   <InspireButton/>
    
    <JournalMenu/>
 
