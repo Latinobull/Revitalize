6bd0d7ee3b40fcbb9d6bb97677fe75a050ff5e98
Donnahue George <Donnahuegjr@gmail.com>
2021-04-22 22:18:37 -0400

    adding links to the pages in the app bar

--- client/src/component/Appbar/index.js
+++ client/src/component/Appbar/index.js
@@ -11,9 +11,9 @@ import FormControlLabel from '@material-ui/core/FormControlLabel';
 import FormGroup from '@material-ui/core/FormGroup';
 import MenuItem from '@material-ui/core/MenuItem';
 import Menu from '@material-ui/core/Menu';
+import { Link } from '@material-ui/core';
 
-
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(theme => ({
   root: {
     flexGrow: 1,
   },
@@ -29,13 +29,15 @@ export default function Appbar() {
   const classes = useStyles();
   const [auth, setAuth] = React.useState(true);
   const [anchorEl, setAnchorEl] = React.useState(null);
+  const [anchorElHome, setAnchorElHome] = React.useState(null);
+  const openHome = Boolean(anchorElHome);
   const open = Boolean(anchorEl);
 
-  const handleChange = (event) => {
+  const handleChange = event => {
     setAuth(event.target.checked);
   };
 
-  const handleMenu = (event) => {
+  const handleMenu = event => {
     setAnchorEl(event.currentTarget);
   };
 
@@ -43,22 +45,68 @@ export default function Appbar() {
     setAnchorEl(null);
   };
 
+  const handleMenuHome = event => {
+    setAnchorElHome(event.currentTarget);
+  };
+
+  const handleCloseHome = event => {
+    setAnchorElHome(null);
+  };
+
   return (
     <div className={classes.root}>
-      <FormGroup>
-      </FormGroup>
+      <FormGroup></FormGroup>
       <AppBar position="static">
         <Toolbar>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            onClick={handleMenuHome}
+            aria-label="menu"
+          >
             <MenuIcon />
           </IconButton>
+          <Menu
+            id="menu-appbar"
+            anchorEl={anchorElHome}
+            anchorOrigin={{
+              vertical: 'top',
+              horizontal: 'right',
+            }}
+            keepMounted
+            transformOrigin={{
+              vertical: 'top',
+              horizontal: 'right',
+            }}
+            open={openHome}
+            onClose={handleCloseHome}
+          >
+            <MenuItem onClick={handleCloseHome}>
+              <Link href="/" color="inherit">
+                Home
+              </Link>
+            </MenuItem>
+            <MenuItem onClick={handleCloseHome}>
+              <Link href="/chat" color="inherit">
+                Chat
+              </Link>
+            </MenuItem>
+          </Menu>
+
           <Typography variant="h6" className={classes.title}>
             Revitalize
           </Typography>
           <FormControlLabel
-          control={<Switch checked={auth} onChange={handleChange} aria-label="login switch" />}
-          label={auth ? 'Logout' : 'Login'}
-        />
+            control={
+              <Switch
+                checked={auth}
+                onChange={handleChange}
+                aria-label="login switch"
+              />
+            }
+            label={auth ? 'Logout' : 'Login'}
+          />
           {auth && (
             <div>
               <IconButton
