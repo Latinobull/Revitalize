0a952691302db99c674c569edd2533740a9eb328
Local Al <alexander.s.woolley@gmail.com>
2021-04-24 19:57:09 -0600

    GET renders in DisplayCard component

--- client/src/component/Journals/DisplayCard/index.js
+++ client/src/component/Journals/DisplayCard/index.js
@@ -1,20 +1,31 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import axios from "axios";
 
-
 export default function displayJournal() {
+  const [data, setData] = useState([]);
 
-  const [data, setData] = useState();
-  
   useEffect(() => {
-    axios.get("api/journals").then(data=>setData(data))
+    axios.get("api/journals").then((data) => {
+      console.log(data);
+      setData(data.data);
+    });
   }, []);
 
-    return (
-      <div>
-         <h6>Feelings: {data.feelings}</h6>
-         <h6>Gratitude: {data.gratitude}</h6>
-         <h6>Thoughts: {data.thoughts}</h6>
-      </div>
-    );
-  }
+  return (
+    <>
+      {data ? (
+        <>
+          {data.map((data) => (
+            <div>
+              <h6>Feelings: {data.feelings}</h6>
+              <h6>Gratitude: {data.gratitude}</h6>
+              <h6>Thoughts: {data.thoughts}</h6>
+            </div>
+          ))}
+        </>
+      ) : (
+        <p>loading...</p>
+      )}
+    </>
+  );
+}
