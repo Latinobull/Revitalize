9618d42352394fd154caba3edee9a080ddb06da3
Local Al <alexander.s.woolley@gmail.com>
2021-04-26 22:33:53 -0600

    bootstrap2

--- client/src/component/Journals/DisplayCard/index.js
+++ client/src/component/Journals/DisplayCard/index.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import "bootstrap/dist/css/bootstrap.css";
-import Carousel from "react-bootstrap/Carousel";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Carousel from 'react-bootstrap/Carousel';
 
 export default function displayJournal() {
   const [data, setData] = useState([]);
