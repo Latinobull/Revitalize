184a87678b66488be04e4bae6842551271660b51
Local Al <alexander.s.woolley@gmail.com>
2021-04-27 11:20:15 -0600

    90% to iterate

--- client/src/component/Journals/Banner/Button.js
+++ client/src/component/Journals/Banner/Button.js
@@ -1,28 +1,21 @@
-import React, { useState } from "react";
-import { makeStyles } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   root: {
-    '& > *': {
+    "& > *": {
       margin: theme.spacing(1),
     },
   },
 }));
 
-const [i, setI] = useState(0);
-
-
-function handleIterate() {
-  setI(i + 1)
-}
-
-export default function InspireButton() {
+export default function InspireButton(props) {
   const classes = useStyles();
 
   return (
     <div className={classes.root}>
-      <Button variant="outlined" onClick={handleIterate} count={i}>
+      <Button variant="outlined" onClick={props.handleIterate}>
         More Inspiration?
       </Button>
     </div>
--- client/src/component/Journals/index.js
+++ client/src/component/Journals/index.js
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, { useState } from "react";
 import JournalMenu from "./JournalMenu";
 import JournalCard from "./Card/index";
 import Banner from "./Banner/index";
 import InspireButton from "./Banner/Button";
 
 
+const [i, setI] = useState(0);
+
+function handleIterate() {
+  setI(i + 1)
+}
+
 function Journals() {
   return (
     <>
-   <Banner/>
-   <InspireButton/>
+   <Banner count={i}/>
+   <InspireButton handleIterate={handleIterate}/>
    
    <JournalMenu/>
 
